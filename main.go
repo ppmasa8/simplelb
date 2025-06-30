0630b56c248eb711c31e3af4ad3463ca44bbdd06
ppmasa8 <matsuzaki_masaya@cyberagent.co.jp>
2025-07-01 01:03:59 +0900

    feat: Add round-robin support for multiple servers

--- main.go
+++ main.go
@@ -6,30 +6,73 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync/atomic"
 )
 
-func main() {
-	backendURL := "http://localhost:8081"
+type Backend struct {
+	URL          *url.URL
+	ReverseProxy *httputil.ReverseProxy
+}
 
-	target, err := url.Parse(backendURL)
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+func (s *ServerPool) GetNextPeer() *Backend {
+	next := int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+	l := len(s.backends) + next
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends)
+		return s.backends[idx]
+	}
+	return nil
+}
+
+func (s *ServerPool) loadBalance(w http.ResponseWriter, r *http.Request) {
+	peer := s.GetNextPeer()
+	if peer != nil {
+		log.Printf("Routing request to: %s", peer.URL.Host)
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
+func NewBackend(serverURL string) (*Backend, error) {
+	u, err := url.Parse(serverURL)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(target)
+	return &Backend{
+		URL:          u,
+		ReverseProxy: httputil.NewSingleHostReverseProxy(u),
+	}, nil
+}
 
-	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, e error) {
-		log.Printf("Proxy error: %v", e)
-		http.Error(w, "Bad Gateway", http.StatusBadGateway)
+func main() {
+	backends := []string{
+		"http://localhost:8081",
+		"http://localhost:8082",
+		"http://localhost:8083",
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Proxying request to %s", target.Host)
-		proxy.ServeHTTP(w, r)
-	})
+	serverPool := &ServerPool{}
+
+	for _, backend := range backends {
+		be, err := NewBackend(backend)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverPool.backends = append(serverPool.backends, be)
+		fmt.Printf("Added backend: %s\n", backend)
+	}
+
+	http.HandleFunc("/", serverPool.loadBalance)
 
 	fmt.Println("Starting proxy server on :8080")
-	fmt.Printf("Proxying to: %s\n", backendURL)
+	fmt.Printf("Configured %d backends\n", len(serverPool.backends))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
